f87bd75c80fcc360f3bc10130a3a691d83a0a27e
Bruno Deutz <brunodeutz@MacBookPro.lan>
2026-04-18 12:26:56 +0200

    Add health check endpoint to fix EB instance termination

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 app.get('/api/pages', (req, res) => {
   res.json(pages);
 });
